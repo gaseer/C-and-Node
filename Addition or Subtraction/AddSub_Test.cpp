4111223bfd5c8d698ec644222300d3fbf8f4ea91
Mohammed Gaseer <85013312+gaseer@users.noreply.github.com>
2022-05-28 05:13:54 +0530

    Create AddSub_Test.cpp

---
+++ Addition or Subtraction/AddSub_Test.cpp
@@ -0,0 +1,19 @@
+//Only created a basic Google test Structure...Not started the testing process
+#include <gtest/gtest.h>
+#define TESTING
+//int ;
+int main()
+{
+	//return;
+}
+#include "Addition or Subtraction.c"
+class MyAppTestSuite : public testing::Test
+{
+	void SetUp(){}
+
+	void TearDown(){}
+};
+
+TEST_F() {
+    EXPECT_EQ(, ());
+}
